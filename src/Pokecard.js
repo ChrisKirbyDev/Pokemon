0615576e47dc51cdee98ff868b92d66ad3b30f09
Chris <chriskirbydev@gmail.com>
2020-08-23 19:55:06 +1200

    Pass props and add Pokemon API

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,9 +1,13 @@
 import React from "react";
+const POKE_API =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-export default function Pokecard() {
+export default function Pokecard(props) {
+  let imgSrc = `${POKE_API}${props.id}.png`;
   return (
-    <div>
-      <h1>PokeCard!</h1>
+    <div className="Pokecard">
+      <h1>{props.name}</h1>
+      <img src={imgSrc} />
     </div>
   );
 }
