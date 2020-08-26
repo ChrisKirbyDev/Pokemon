c81d9a45d396069428205c5b32c8ae801d4862ff
Chris <chriskirbydev@gmail.com>
2020-08-27 11:03:54 +1200

    Update api, use slice method for ID to render img

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Pokecard.css";
-const POKE_API =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 export default function Pokecard(props) {
-  let imgSrc = `${POKE_API}${props.id}.png`;
+  let imgSrc = `${POKE_API}${padToThree(props.id)}.png`;
   return (
     <div className="Pokecard">
       <h1 className="Pokecard-title">{props.name}</h1>
