6f96b9ccffc28bc97ef8296eeb7bc367acfa9623
Chris <chriskirbydev@gmail.com>
2020-08-28 10:55:15 +1200

    Create Pokegame class component

---
+++ src/Pokegame.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Pokecard from "./Pokecard";
+import "./Pokedex.css";
+
+class Pokedex extends Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default Pokegame;
